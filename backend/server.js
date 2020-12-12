68008b370ca008dc09897ee7a084c2fdc832c38d
unknown <ahmaat19@gmail.com>
2020-12-12 12:22:12 +0300

    Changed post

--- backend/server.js
+++ backend/server.js
@@ -27,11 +27,26 @@ app.use('/api/blood-store', bloodStoreRoutes)
 app.use('/api/blood-request', bloodRequestRoutes)
 app.use('/api/blood-issue', bloodIssueRoutes)
 
+// Set static folder
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 app.use(notFound)
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 80
 
 app.listen(
   PORT,
